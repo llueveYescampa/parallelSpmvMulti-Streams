a2cebe3c0a88a4d0988922bbbd656a775d55a908
Edgar F. Black <edgarfblack@gmail.com>
2020-07-01 10:47:28 -0500

    fixed the non-zeros distribution routine

--- src/reader/getRowsNnzPerStream.cu
+++ src/reader/getRowsNnzPerStream.cu
@@ -1,6 +1,35 @@
+#define LOW(id,p,n)  ((id)*(n)/(p))
+#define HIGH(id,p,n) (LOW((id)+1,p,n)-1)
+#define SIZE(id,p,n) (LOW((id)+1,p,n)-LOW(id,p,n)) // eblack
 
-void getRowsNnzPerStream(int *rowsPerStream, const int *global_n, const int *global_nnz,  const int *row_Ptr, const int nRowBlocks)
+void getRowsNnzPerStream(int *rowsPerSet, const int *global_n, const int *global_nnz,  const int *rows, const int nRowBlocks)
 {
+    int lowRow=0, upRow;
+    int reducedBlockSize= nRowBlocks;
+    int reducedNnz=*global_nnz;
+    int nnzLimit = rows[lowRow] + SIZE(0,reducedBlockSize, reducedNnz);
+    int partition=0;    
+
+    for (int row = 0; row<*global_n; ++row) {
+        if ( rows[row+1] >=  nnzLimit ) { 
+            if ( ( rows[row+1] - nnzLimit)  <=  nnzLimit - rows[row]   ) {
+                upRow = row;
+            } else {
+                upRow = row-1;
+            } // end if //
+            rowsPerSet[partition] = upRow-lowRow+1;
+            reducedNnz -=  (rows[upRow+1]-rows[lowRow]);
+            --reducedBlockSize;
+            lowRow=upRow+1;
+            if (partition < nRowBlocks-1 ) nnzLimit= rows[lowRow] + SIZE(0,reducedBlockSize, reducedNnz);
+            ++partition;
+        } // end if // 
+        
+    } // end for //
+} // end of getRowsPerProc //
+
+/*
+
     double nnzIncre = (double) *global_nnz/ (double) nRowBlocks;
     double lookingFor=nnzIncre;
     int startRow=0, endRow;
@@ -27,4 +56,6 @@ void getRowsNnzPerStream(int *rowsPerStream, const int *global_n, const int *glo
             } // end if //   
         } // end if // 
     } // end for //
-} // end of getRowsPerProc //
+
+*/
+
