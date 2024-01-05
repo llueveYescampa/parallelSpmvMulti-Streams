a9a3a2b4f7fcfb6587399c4485d102d1e1d69b7c
Edgar F. Black <edgarfblack@gmail.com>
2024-01-05 11:43:09 -0600

    spmv kernel modified

--- src/spmv/spmv.cu
+++ src/spmv/spmv.cu
@@ -69,166 +69,69 @@ void spmv(       floatType *__restrict__       y,
     temp[sharedMemIndx] = static_cast<floatType>(0.0);
 
     if (row < nRows) {
+        for (int col=row_ptr[row]+threadIdx.x; col < row_ptr[row+1]; col+=blockDim.x) {
+          #ifdef USE_TEXTURE
+          temp[sharedMemIndx] += (val[col] * fetch_floatType( xTex, col_idx[col]));
+          #else
+          temp[sharedMemIndx] += (val[col] * x[col_idx[col]]);
+          #endif
+        } // end for //
+
         switch(blockDim.x) {
-            case 1  :
-                for (int col=row_ptr[row]+threadIdx.x; col < row_ptr[row+1]; col+=blockDim.x) {
-                    #ifdef USE_TEXTURE
-                    temp[sharedMemIndx] += (val[col] * fetch_floatType( xTex, col_idx[col]));
-                    #else
-                    temp[sharedMemIndx] += (val[col] * x[col_idx[col]]);
-                    #endif
-                } // end for //
-
-                if ((sharedMemIndx % blockDim.x)  == 0) {
-                    //y[row] += temp[sharedMemIndx];
-                    y[row] =  beta * y[row] + alpha*temp[sharedMemIndx];
-                } // end if //   
-                break;
             case 2  :
-                for (int col=row_ptr[row]+threadIdx.x; col < row_ptr[row+1]; col+=blockDim.x) {
-                    #ifdef USE_TEXTURE
-                    temp[sharedMemIndx] += (val[col] * fetch_floatType( xTex, col_idx[col]));
-                    #else
-                    temp[sharedMemIndx] += (val[col] * x[col_idx[col]]);
-                    #endif
-                } // end for //
                 if (threadIdx.x <  1) warpReduce< 2>(&temp[sharedMemIndx]);
-
-                if ((sharedMemIndx % blockDim.x)  == 0) {
-                    //y[row] += temp[sharedMemIndx];
-                    y[row] =  beta * y[row] + alpha*temp[sharedMemIndx];
-                } // end if //   
                 break; 
             case 4  :
-                for (int col=row_ptr[row]+threadIdx.x; col < row_ptr[row+1]; col+=blockDim.x) {
-                    #ifdef USE_TEXTURE
-                    temp[sharedMemIndx] += (val[col] * fetch_floatType( xTex, col_idx[col]));
-                    #else
-                    temp[sharedMemIndx] += (val[col] * x[col_idx[col]]);
-                    #endif
-                } // end for //
                 if (threadIdx.x <  2) warpReduce< 4>(&temp[sharedMemIndx]);
-
-                if ((sharedMemIndx % blockDim.x)  == 0) {
-                    //y[row] += temp[sharedMemIndx];
-                    y[row] =  beta * y[row] + alpha*temp[sharedMemIndx];
-                } // end if //   
                 break; 
             case 8  :
-                for (int col=row_ptr[row]+threadIdx.x; col < row_ptr[row+1]; col+=blockDim.x) {
-                    #ifdef USE_TEXTURE
-                    temp[sharedMemIndx] += (val[col] * fetch_floatType( xTex, col_idx[col]));
-                    #else
-                    temp[sharedMemIndx] += (val[col] * x[col_idx[col]]);
-                    #endif
-                } // end for //
                 if (threadIdx.x <  4) warpReduce< 8>(&temp[sharedMemIndx]);
-
-                if ((sharedMemIndx % blockDim.x)  == 0) {
-                    //y[row] += temp[sharedMemIndx];
-                    y[row] =  beta * y[row] + alpha*temp[sharedMemIndx];
-                } // end if //   
                 break; 
             case 16  :
-                for (int col=row_ptr[row]+threadIdx.x; col < row_ptr[row+1]; col+=blockDim.x) {
-                    #ifdef USE_TEXTURE
-                    temp[sharedMemIndx] += (val[col] * fetch_floatType( xTex, col_idx[col]));
-                    #else
-                    temp[sharedMemIndx] += (val[col] * x[col_idx[col]]);
-                    #endif
-                } // end for //
                 if (threadIdx.x <  8) warpReduce<16>(&temp[sharedMemIndx]);
-
-                if ((sharedMemIndx % blockDim.x)  == 0) {
-                    //y[row] += temp[sharedMemIndx];
-                    y[row] =  beta * y[row] + alpha*temp[sharedMemIndx];
-                } // end if //   
                 break;
             case 32  :
-                for (int col=row_ptr[row]+threadIdx.x; col < row_ptr[row+1]; col+=blockDim.x) {
-                    #ifdef USE_TEXTURE
-                    temp[sharedMemIndx] += (val[col] * fetch_floatType( xTex, col_idx[col]));
-                    #else
-                    temp[sharedMemIndx] += (val[col] * x[col_idx[col]]);
-                    #endif
-                } // end for //
                 if (threadIdx.x < 16) warpReduce<32>(&temp[sharedMemIndx]);
-
-                if ((sharedMemIndx % blockDim.x)  == 0) {
-                    //y[row] += temp[sharedMemIndx];
-                    y[row] =  beta * y[row] + alpha*temp[sharedMemIndx];
-                } // end if //   
                 break; 
             case 64  :
-                for (int col=row_ptr[row]+threadIdx.x; col < row_ptr[row+1]; col+=blockDim.x) {
-                    #ifdef USE_TEXTURE
-                    temp[sharedMemIndx] += (val[col] * fetch_floatType( xTex, col_idx[col]));
-                    #else
-                    temp[sharedMemIndx] += (val[col] * x[col_idx[col]]);
-                    #endif
-                } // end for //
                __syncthreads();
                 if (threadIdx.x < 32) warpReduce<64>(&temp[sharedMemIndx]);
-
-                if ((sharedMemIndx % blockDim.x)  == 0) {
-                    //y[row] += temp[sharedMemIndx];
-                    y[row] =  beta * y[row] + alpha*temp[sharedMemIndx];
-                } // end if //   
                 break; 
             case 128  :
-                for (int col=row_ptr[row]+threadIdx.x; col < row_ptr[row+1]; col+=blockDim.x) {
-                    #ifdef USE_TEXTURE
-                    temp[sharedMemIndx] += (val[col] * fetch_floatType( xTex, col_idx[col]));
-                    #else
-                    temp[sharedMemIndx] += (val[col] * x[col_idx[col]]);
-                    #endif
-                } // end for //
                __syncthreads();
-                if (threadIdx.x < 64) { temp[sharedMemIndx] += temp[sharedMemIndx +  64]; __syncthreads(); }
+                if (threadIdx.x < 64) temp[sharedMemIndx] += temp[sharedMemIndx +  64];
+                
+                __syncthreads();
                 if (threadIdx.x < 32) warpReduce<64>(&temp[sharedMemIndx]);
-
-                if ((sharedMemIndx % blockDim.x)  == 0) {
-                    //y[row] += temp[sharedMemIndx];
-                    y[row] =  beta * y[row] + alpha*temp[sharedMemIndx];
-                } // end if //   
                 break; 
             case 256  :
-                for (int col=row_ptr[row]+threadIdx.x; col < row_ptr[row+1]; col+=blockDim.x) {
-                    #ifdef USE_TEXTURE
-                    temp[sharedMemIndx] += (val[col] * fetch_floatType( xTex, col_idx[col]));
-                    #else
-                    temp[sharedMemIndx] += (val[col] * x[col_idx[col]]);
-                    #endif
-                } // end for //
                __syncthreads();
-                if (threadIdx.x<128) { temp[sharedMemIndx] += temp[sharedMemIndx + 128]; __syncthreads(); }
-                if (threadIdx.x< 64) { temp[sharedMemIndx] += temp[sharedMemIndx +  64]; __syncthreads(); }
+                if (threadIdx.x<128) temp[sharedMemIndx] += temp[sharedMemIndx + 128]; 
+                
+                __syncthreads();
+                if (threadIdx.x< 64) temp[sharedMemIndx] += temp[sharedMemIndx +  64]; 
+                
+                __syncthreads();
                 if (threadIdx.x < 32) warpReduce<64>(&temp[sharedMemIndx]);
-
-                if ((sharedMemIndx % blockDim.x)  == 0) {
-                    //y[row] += temp[sharedMemIndx];
-                    y[row] =  beta * y[row] + alpha*temp[sharedMemIndx];
-                } // end if //   
                 break; 
             case 512  :
-                for (int col=row_ptr[row]+threadIdx.x; col < row_ptr[row+1]; col+=blockDim.x) {
-                    #ifdef USE_TEXTURE
-                    temp[sharedMemIndx] += (val[col] * fetch_floatType( xTex, col_idx[col]));
-                    #else
-                    temp[sharedMemIndx] += (val[col] * x[col_idx[col]]);
-                    #endif
-                } // end for //
                __syncthreads();
-                if (threadIdx.x<256) { temp[sharedMemIndx] += temp[sharedMemIndx + 256]; __syncthreads(); }
-                if (threadIdx.x<128) { temp[sharedMemIndx] += temp[sharedMemIndx + 128]; __syncthreads(); }
-                if (threadIdx.x< 64) { temp[sharedMemIndx] += temp[sharedMemIndx +  64]; __syncthreads(); }
+                if (threadIdx.x<256) temp[sharedMemIndx] += temp[sharedMemIndx + 256]; 
+                
+                __syncthreads();
+                if (threadIdx.x<128) temp[sharedMemIndx] += temp[sharedMemIndx + 128]; 
+                
+                __syncthreads();
+                if (threadIdx.x< 64) temp[sharedMemIndx] += temp[sharedMemIndx +  64]; 
+                
+                __syncthreads();
                 if (threadIdx.x < 32) warpReduce<64>(&temp[sharedMemIndx]);
-
-                if ((sharedMemIndx % blockDim.x)  == 0) {
-                    //y[row] += temp[sharedMemIndx];
-                    y[row] =  beta * y[row] + alpha*temp[sharedMemIndx];
-                } // end if //   
                 break; 
         } // end switch //
+        if ( (sharedMemIndx % blockDim.x)  == 0) {
+            //y[row] += temp[sharedMemIndx];
+            y[row] =  beta * y[row] + alpha*temp[sharedMemIndx];
+        } // end if //        
     } // end if    
+    
 } // end of spmv() //
